bd55892c2193d6c14a76d97403aa43f5e1cd79b7
JungWhoNam <jungwhonam@gmail.com>
2022-02-08 21:58:56 +0900

    Add a feature to rotate a camera

--- Assets/CameraMotions/PanMotion.cs
+++ Assets/CameraMotions/PanMotion.cs
@@ -2,33 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PanMotion : MonoBehaviour
+namespace CameraMotions
 {
-    public Camera movingCamera;
-    public Transform from;
-    public Transform to;
-
-    [Range(0.0f, 1.0f)]
-    public float progress;
-    [Range(0.0f, 1.0f)]
-    public float incAmtPerSes = 0.1f;
-    public bool loop = false;
-
-    private void Update()
+    public class PanMotion : MonoBehaviour
     {
-        progress += incAmtPerSes * Time.deltaTime;
-        if (loop) progress = progress > 1f ? 0 : progress;
-        else progress = Mathf.Clamp01(progress);
+        public Camera movingCamera;
+        public Transform from;
+        public Transform to;
 
-        UpdateCamera(progress);
+        [Range(0.0f, 1.0f)]
+        public float progress;
+        [Range(0.0f, 1.0f)]
+        public float incAmtPerSes = 0.1f;
+        public bool loop = false;
+
+        private void Update()
+        {
+            progress += incAmtPerSes * Time.deltaTime;
+            if (loop) progress = progress > 1f ? 0 : progress;
+            else progress = Mathf.Clamp01(progress);
+
+            UpdateCamera(progress);
+        }
+
+        private void UpdateCamera(float progress)
+        {
+            (movingCamera == null ? Camera.main : movingCamera).
+                transform.SetPositionAndRotation(
+                    Vector3.Lerp(from.position, to.position, progress),
+                    Quaternion.Lerp(from.rotation, to.rotation, progress)
+                );
+        }
     }
 
-    private void UpdateCamera(float progress)
-    {
-        (movingCamera == null ? Camera.main : movingCamera).
-            transform.SetPositionAndRotation(
-                Vector3.Lerp(from.position, to.position, progress),
-                Quaternion.Lerp(from.rotation, to.rotation, progress)
-            );
-    }
 }
---
+++ Assets/CameraMotions/RotateMotion.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CameraMotions
+{
+    public class RotateMotion : MonoBehaviour
+    {
+        public Camera movingCamera;
+        public Transform rotateAround;
+
+        [Range(0.0f, 180.0f)]
+        public float incAmtPerSes = 36f;
+
+        private void Update()
+        {
+            UpdateCamera(incAmtPerSes * Time.deltaTime);
+        }
+
+        private void UpdateCamera(float intAmt)
+        {
+            (movingCamera == null ? Camera.main : movingCamera).
+                transform.RotateAround(
+                    rotateAround.transform.position,
+                    rotateAround.transform.up,
+                    intAmt
+                );
+        }
+    }
+}
